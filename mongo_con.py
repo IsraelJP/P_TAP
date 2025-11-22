efb21b8ec48a869de46b58709d4b4904e2c35e17
Israel <L22281378@toluca.tecnm.mx>
2025-11-22 12:44:51 -0600

    Implementa un Singleton para la conexión a MongoDB
    Refactoriza la lógica de conexión a la base de datos para usar el patrón de diseño Singleton.
    
    Esto asegura que solo se cree una instancia de la conexión, mejorando la gestión de recursos. La nueva clase encapsula la configuración y proporciona un método para obtener colecciones fácilmente.

--- mongo_con.py
+++ mongo_con.py
@@ -1,11 +1,28 @@
+# connection.py
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://admin:EwUMEAJtTFAvpe4Y@cluster.zpxcapr.mongodb.net/?appName=Cluster"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+
+
+class MongoConnection:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(MongoConnection, cls).__new__(cls)
+
+            uri = "mongodb+srv://admin:EwUMEAJtTFAvpe4Y@cluster.zpxcapr.mongodb.net/?appName=Cluster"
+
+            try:
+                cls._instance.client = MongoClient(uri, server_api=ServerApi('1'))
+                cls._instance.client.admin.command("ping")
+                print("Conexión exitosa a MongoDB Atlas.")
+            except Exception as e:
+                print("Error de conexión:", e)
+
+
+            cls._instance.db = cls._instance.client["proyecto"]
+
+        return cls._instance
+
+    def get_collection(self, name: str):
+        """Regresa una colección lista para usarse."""
+        return self.db[name]
